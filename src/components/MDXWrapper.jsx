5569d09ae2dc870637d9e5483f0c102baa7341fd
Sydney Baroya <syd.bar97@gmail.com>
2025-08-26 18:25:39 -0700

    underline links in project pages and not in other pages

--- src/components/MDXWrapper.jsx
+++ src/components/MDXWrapper.jsx
@@ -12,14 +12,9 @@ const components = {
     href?.startsWith("/") ? (
       <Link href={href} className="text-blue-500 underline">{children}</Link>
     ) : (
-      <a href={href} target="_blank" rel="noopener noreferrer" className="underline">{children}</a>
+      <a href={href} target="_blank" rel="noopener noreferrer" style={{ textDecoration: "underline"}}>{children}</a>
     ),
   MdxImage: (props) => <MdxImage {...props} />,
-  CustomButton: ({ href, children }) => (
-    <a href={href} className="bg-blue-600 text-white px-4 py-2 rounded-lg shadow hover:bg-blue-700">
-      {children}
-    </a>
-  ),
   motion: motion,
   Stack: Stack,
   Grid: Grid,
