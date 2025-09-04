b9305b821bbc535391d2f9d71e9f56511ce1df46
Sydney Baroya <syd.bar97@gmail.com>
2025-09-04 16:39:21 -0700

    theme toggle button on hover update

--- src/components/themeToggle.jsx
+++ src/components/themeToggle.jsx
@@ -4,12 +4,23 @@ import { IconButton } from '@mui/material';
 import { useColorMode } from '@/app/themeRegistry';
 import LightModeIcon from '@mui/icons-material/LightModeRounded';
 import DarkModeIcon from '@mui/icons-material/DarkModeRounded';
+import { useTheme } from '@mui/material/styles';
 
 export default function ThemeToggle() {
   const { mode, toggleColorMode } = useColorMode();
+  const theme = useTheme();
 
   return (
-    <IconButton onClick={toggleColorMode} color={'primary'}>
+    <IconButton
+      onClick={toggleColorMode}
+      color={'primary'}
+      sx={{
+        '&:hover': {
+          backgroundColor: theme.palette.primary.main,
+          color: theme.palette.secondary.main,
+        },
+      }}
+    >
       {mode === 'light' ? <DarkModeIcon /> : <LightModeIcon />}
     </IconButton>
   );
